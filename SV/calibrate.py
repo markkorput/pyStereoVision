9621f85868c7c39febab9831460177e1e8b8666b
mark <mark.korput@gmail.com>
2019-07-17 09:03:22 +0200

    calibrate.py finding and showing chess pattern (seems not perfect yet though)

--- SV/calibrate.py
+++ SV/calibrate.py
@@ -5,7 +5,8 @@ from optparse import OptionParser
 
 DEFAULTS = {
   'video': None,
-  'delay': None
+  'delay': None,
+  'x_amount': 9, 'y_amount': 6
 }
 
 def parse_args():
@@ -18,6 +19,14 @@ def parse_args():
                     help="Delay between each frame",
                     default=DEFAULTS['delay'])
 
+  parser.add_option("-x", "--x-amount", dest="x_amount", type="int",
+                    help="Number of columns in the chessboard pattern",
+                    default=DEFAULTS['x_amount'])
+
+  parser.add_option("-y", "--y-amount", dest="y_amount", type="int",
+                    help="Number of rows in the chessboard pattern",
+                    default=DEFAULTS['y_amount'])
+
   (options, args) = parser.parse_args()
   return (options, args)
 
@@ -25,15 +34,36 @@ def get_LR_filenames(filename):
   filename, ext = os.path.splitext(filename)
   return (filename+'_L'+ext, filename+'_R'+ext)  
 
-def get_stream(vid_path):
+def get_stream(vid_path, pattern_dimm):
   cap = cv2.VideoCapture(vid_path)
-  return {'cap': cap, 'ID': vid_path}
+  return {'cap': cap, 'ID': vid_path, 'pattern_dimm': pattern_dimm}
 
 def update(streams):
   for s in streams:
     (retval, frame) = s['cap'].read()
     if retval:
       cv2.imshow(s['ID'], frame)
+
+      ret, corners = get_corners(frame, s['pattern_dimm'])
+
+      if ret:
+        overlay_img = cv2.drawChessboardCorners(frame, (7,6), corners, ret)
+        cv2.imshow(s['ID'], overlay_img)
+
+def get_corners(img, pattern_dimm):
+  gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+  # Find the chess board corners
+  ret, corners = cv2.findChessboardCorners(gray, pattern_dimm, None)
+
+  # termination criteria
+  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+  # If found, add object points, image points (after refining them)
+  if ret == True:
+    return ret, cv2.cornerSubPix(gray,corners,pattern_dimm,(-1,-1),criteria)
+  return ret, corners
+
 
 if __name__ == '__main__':
   (options, args) = parse_args()
@@ -44,8 +74,8 @@ if __name__ == '__main__':
   if options.video:
     save_path_L, save_path_R = get_LR_filenames(options.video)
     print('Using videos: {}, {}'.format(save_path_L,save_path_R))
-    streams.append(get_stream(save_path_L))
-    streams.append(get_stream(save_path_R))
+    streams.append(get_stream(save_path_L, (options.x_amount, options.y_amount)))
+    streams.append(get_stream(save_path_R, (options.x_amount, options.y_amount)))
 
   print("Starting calibration, press 'Q' or CTRL+C to stop...")
   try:
