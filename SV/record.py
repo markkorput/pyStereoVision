d3f657985090b56aee84bff81e327343d6ec2efc
mark <mark.korput@gmail.com>
2019-10-12 18:29:06 +0200

    record gets time-sync option

--- SV/record.py
+++ SV/record.py
@@ -1,69 +1,176 @@
 # original logic taken from:
 # https://github.com/codingforentrepreneurs/OpenCV-Python-Series/blob/master/src/lessons/record-video.py
 
-import os
-# import numpy as np
-import cv2
+import os, logging, cv2, time, math
 from optparse import OptionParser
 
 DEFAULTS = {
-  'filename': 'saved-media/video.avi', # .avi .mp4
+  'input': "0,1",
+  'output': 'saved-media/recording-L.avi,saved-media/recording-R.avi',
+
+  # 'filename': 'saved-media/video.avi', # .avi .mp4
   'fps': 24.0,
   'res': '720p', # 480p | 720p | 1080p | 4k
   'res_choices': ('480p', '720p', '1080p', '4k'),
-  'camL': 0,
-  'camR': 1,
+  # 'camL': 0,
+  # 'camR': 1,
   'show': True
 }
 
-# Set resolution for the video capture
-# Function adapted from https://kirr.co/0l6qmh
-def change_res(cap, width, height):
-    cap.set(3, width)
-    cap.set(4, height)
+# A stream is an input and and output
+class Stream:
+  def __init__(self, input, output, res, fps):
+    self.input = input
+    self.output = output
+    self.res = res
+    self.fps = fps
 
-# Standard Video Dimensions Sizes
-STD_DIMENSIONS =  {
-    "480p": (640, 480),
-    "720p": (1280, 720),
-    "1080p": (1920, 1080),
-    "4k": (3840, 2160),
-}
+    self.cap = cv2.VideoCapture(int(self.input) if self.input.isdigit() else self.input)
 
-def get_dims(cap, res='1080p'):
-    width, height = STD_DIMENSIONS['480p']
-    if res in STD_DIMENSIONS:
-        width, height = STD_DIMENSIONS[res]
-    change_res(cap, width, height)
-    return width, height
+    STD_DIMENSIONS =  {
+      "480p": (640, 480),
+      "720p": (1280, 720),
+      "1080p": (1920, 1080),
+      "4k": (3840, 2160)
+    }
 
+    self.dimms = STD_DIMENSIONS['480p']
+    if self.res in STD_DIMENSIONS:
+        self.dimms = STD_DIMENSIONS[self.res]
 
+    self.cap.set(3, self.dimms[0])
+    self.cap.set(4, self.dimms[1])
 
-# Video Encoding, might require additional installs
-# Types of Codes: http://www.fourcc.org/codecs.php
-VIDEO_TYPE = {
-    'avi': cv2.VideoWriter_fourcc(*'XVID'),
-    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
-    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
-}
+    # Video Encoding, might require additional installs
+    # Types of Codes: http://www.fourcc.org/codecs.php
+    VIDEO_TYPE = {
+        'avi': cv2.VideoWriter_fourcc(*'XVID'),
+        #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+        'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+    }
 
-def get_video_type(filename):
-  filename, ext = os.path.splitext(filename)
-  if ext in VIDEO_TYPE:
-    return VIDEO_TYPE[ext]
-  return VIDEO_TYPE['avi']
+    filename, ext = os.path.splitext(self.output)
+    typ = VIDEO_TYPE[ext] if ext in VIDEO_TYPE else VIDEO_TYPE['avi']
+    self.writer = cv2.VideoWriter(self.output, typ, self.fps, self.dimms)
 
+    self.lastframe = None
+    self.firstWriteTime = None
+    self.writeFrameCount = 0
 
+  def __del__(self):
+    if self.cap:
+      logging.debug("Capture for {}".format(self.input))
+      self.cap.release()
+      self.cap = None
 
-def get_LR_filenames(filename):
-  filename, ext = os.path.splitext(filename)
-  return (filename+'_L'+ext, filename+'_R'+ext)  
+    if self.writer:
+      logging.debug("Releasing writer for {}".format(self.output))
+      self.writer.release()
+      self.writer = None
+
+  def read(self):
+    # self.lastframe = s.cap.read()[1]
+    ret,frame = self.cap.read()
+    if ret:
+      self.lastframe = frame
+
+  def write(self, grayscale=False, sync=True):
+    frame = self.lastframe
+    # Convert to grayscale?
+    if grayscale:
+      frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # write frame
+    self.writer.write(frame)
+    self.writeFrameCount += 1
+
+    if sync:
+      if not self.firstWriteTime:
+        self.firstWriteTime = time.time()
+        return
+
+      rectime = time.time() - self.firstWriteTime
+      syncFrameCount = rectime * self.fps
+
+      while self.writeFrameCount < syncFrameCount:
+        logging.debug('Writing padding frame for syncing')
+        self.writer.write(frame)
+        self.writeFrameCount += 1
+
+def main(input="0,1", output="saved-media/record-L.avi,saved-media/record-R.avi", res='720p', fps=25, grayscale=False, show=True, verbose=False, sync=False):
+  logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO, format='%(asctime)s %(message)s')
+
+  ##### PRE-CHECK #####
+
+  inputs = input.split(',')
+  outputs = output.split(',')
+
+  if len(inputs) != len(outputs):
+    logging.warning('Number of inputs ({}: {}) doesn\'t match with number of outputs ({}: {})'.format(len(inputs), inputs, len(outputs), outputs))
+    return
+
+  ##### INIT #####
+
+  streams = []
+
+  for idx, input in enumerate(inputs):
+    stream = Stream(input, outputs[idx], res, fps)
+    streams.append(stream)
+
+  ##### RECORD #####
+
+  counter = 0
+
+  logging.info("Starting recording, press <ESC>, 'Q' or CTRL+C to stop...")
+  try:
+    while(True):
+        ## READ ##
+        for s in streams:
+          s.read()
+
+        ## WRITE ##
+        for s in streams:
+          s.write(grayscale=grayscale, sync=sync)
+
+        counter += 1
+        print("Number of frames recorded: {}\r".format(counter), end='')
+
+        # Display the resulting frame
+        if show:
+          for s in streams:
+            cv2.imshow(s.input, s.lastframe)
+
+        key = cv2.waitKey(3) & 0xFF
+
+        if key == ord('q') or key == 27:
+          break
+
+  except KeyboardInterrupt:
+    logging.debug("KeyboardInterrupt, stopping")
+
+  logging.info('Total number of frames recorded: {}'.format(counter))
+
+  ##### CLEANUP #####
+
+  cv2.destroyAllWindows()
 
 if __name__ == '__main__':
   parser = OptionParser()
-  parser.add_option("-o", "--out-file", dest="FILENAME",
-                    help="Video file to write to", metavar="FILENAME",
-                    default=DEFAULTS['filename'])
+  parser.add_option("-o", "--output", dest="output",
+                    help="Video file(s) to write to", metavar="FILENAMES",
+                    default=DEFAULTS['output'])
+
+  parser.add_option("-i", "--input", dest="input",
+                    help="Video stream(s) to read from to", metavar="FILENAMES",
+                    default=DEFAULTS['input'])
+
+  # parser.add_option("-l", "--left", dest="CAM_L", type="int",
+  #                   help="Camera ID for LEFT eye Camera", metavar="LEFT",
+  #                   default=DEFAULTS['camL'])
+
+  # parser.add_option("-r", "--right", dest="CAM_R", type="int",
+  #                   help="Camera ID for RIGHT eye Camera", metavar="RIGHT",
+  #                   default=DEFAULTS['camR'])
 
   parser.add_option("-d", "--dimensions", dest="RES", type="choice",
                     choices=DEFAULTS['res_choices'],
@@ -74,86 +181,23 @@ if __name__ == '__main__':
                     help="Frames Per Second", metavar="FPS",
                     default=DEFAULTS['fps'])
 
-  parser.add_option("-l", "--left", dest="CAM_L", type="int",
-                    help="Camera ID for LEFT eye Camera", metavar="LEFT",
-                    default=DEFAULTS['camL'])
-
-  parser.add_option("-r", "--right", dest="CAM_R", type="int",
-                    help="Camera ID for RIGHT eye Camera", metavar="RIGHT",
-                    default=DEFAULTS['camR'])
 
   parser.add_option("-s", "--show",
                     action="store_false", dest="SHOW", default=DEFAULTS['show'],
                     help="Show recorded frames")
 
-  (options, args) = parser.parse_args()
+  parser.add_option("-g", "--grayscale",
+                    action="store_true", dest="grayscale", default=False,
+                    help="Record grayscale")
 
-  save_path_L, save_path_R = get_LR_filenames(options.FILENAME)
+  parser.add_option("-t", "--time-sync",
+                    action="store_true", dest="sync", default=False,
+                    help="Record additional frames if time syncing requires it")
 
-  print("Config:")  
-  print("Cameras (left/right): {}/{}".format(options.CAM_L, options.CAM_R))
-  print("Out files (left/right): {}, {}".format(save_path_L, save_path_R))
-  print("Resolution: {}".format(options.RES))
-  print("FPS: {}".format(options.FPS))
+  parser.add_option("-v", "--verbose",
+                    action="store_true", dest="verbose", default=False,
+                    help="Verbose logging")
 
-  def get_stream(camId, res, fps, filepath):
-    cap = cv2.VideoCapture(camId)
-    dims = get_dims(cap, res=res)
-    video_type_cv2 = get_video_type(filepath)
-    writer = cv2.VideoWriter(filepath, video_type_cv2, fps, dims)
-    return {'cap': cap, 'dims': dims, 'vidtype': video_type_cv2, 'writer': writer, 'ID': filepath}
+  (opts, args) = parser.parse_args()
 
-  streams = []
-  if not options.CAM_L == -1:
-    streams.append(
-      get_stream(options.CAM_L,
-        options.RES,
-        options.FPS,
-        save_path_L))
-
-  if not options.CAM_R == -1:
-    streams.append(
-      get_stream(options.CAM_R,
-        options.RES,
-        options.FPS,
-        save_path_R))
-
-  counter = 0
-
-  print("Starting recording, press 'Q' or CTRL+C to stop...")
-  try:
-    while(True):
-        # Capture frame-by-frame
-        # ret, frame = capL.read()
-        for s in streams:
-          s['lastframe'] = s['cap'].read()[1]
-
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # outL.write(frame)
-        for s in streams:
-          s['writer'].write(s['lastframe'])
-          print("Wrote {} lines".format(len(s['lastframe'])))
-
-        counter += 1
-        print ("Recorded frame {}".format(counter))
-
-        # Display the resulting frame
-        if options.SHOW:
-          for s in streams:
-            cv2.imshow(s['ID'], s['lastframe'])
-
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break
-
-  except KeyboardInterrupt:
-    print("KeyboardInterrupt, stopping")
-
-
-  # When everything done, release the capture
-  # cap.release()
-  # out.release()
-  for s in streams:
-    s['cap'].release()
-    s['writer'].release()
-
-  cv2.destroyAllWindows()
+  main(input=opts.input, output=opts.output, res=opts.RES, fps=opts.FPS, show=opts.SHOW, grayscale=opts.grayscale, verbose=opts.verbose, sync=opts.sync)
