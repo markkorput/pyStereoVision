c2d482589dcc98d651a6c701bf7ebeb9b90cebf8
mark <mark.korput@gmail.com>
2019-10-18 01:04:15 +0200

    params submodule

---
+++ SV/params/OscServer.py
@@ -0,0 +1,196 @@
+from .Params import Params, toDict
+import threading, logging, re
+from evento import Event
+
+
+DEPS = None # {'osc_server': None, 'dispatcher': None}
+
+def loadDeps():
+  result = {}
+  try:
+    from pythonosc import dispatcher
+    from pythonosc import osc_server
+    result['osc_server'] = osc_server
+    result['dispatcher'] = dispatcher
+  except ImportError:
+    from cfgr.embeds.python2osc import dispatcher
+    from cfgr.embeds.python2osc import osc_server
+    result['osc_server'] = osc_server
+    result['dispatcher'] = dispatcher
+  except Exception:
+    from cfgr.embeds.python2osc import dispatcher
+    from cfgr.embeds.python2osc import osc_server
+    result['osc_server'] = osc_server
+    result['dispatcher'] = dispatcher
+
+    # logging.getLogger(__name__).warning("failed to load pythonosc dependency; OscServer component will not work")
+    pass
+
+  return result
+
+class OscServer:
+  def __init__(self, port=8080, host=''):
+    self.params = params
+    self.host = host
+    self.port = port
+    self.server = None
+    self.thread = None
+    self.isConnected = False
+
+    self.connectedEvent = Event()
+    self.disconnectedEvent = Event()
+    self.messageEvent = Event()
+
+  def __del__(self):
+    self.stop()
+
+  def start(self):
+    global DEPS
+
+    if self.isConnected:
+      return False
+
+    if DEPS == None:
+      DEPS = loadDeps()
+
+      if not 'osc_server' in DEPS or not 'dispatcher' in DEPS:
+        print('[OscServer] could not load dependencies')
+
+    if not 'osc_server' in DEPS or not 'dispatcher' in DEPS:
+      return False
+
+    disp = DEPS['dispatcher'].Dispatcher()
+    disp.map("*", self._onOscMsg)
+    # disp.map("/logvolume", print_compute_handler, "Log volume", math.log)
+
+    result = False
+    try:
+      self.server = DEPS['osc_server'].ThreadingOSCUDPServer((self.host, self.port), disp)
+      self.server.daemon_threads = True
+      self.server.timeout = 1.0
+      # self.server = DEPS['osc_server'].BlockingOSCUDPServer((self.host, self.port), disp)
+      def threadFunc():
+          try:
+              self.server.serve_forever()
+          except KeyboardInterrupt:
+              pass
+          self.server.server_close()
+
+      self.thread = threading.Thread(target = threadFunc);
+      self.thread.start()
+      # set internal connected flag
+      result = True
+    except OSError as err:
+      print("[OscServer] Could not start OSC server: "+str(err))
+
+    if result:
+      # notify
+      logging.debug("[OscServer {0}:{1}] server started".format(self.host, str(self.port)))
+      self.connectedEvent(self)
+
+    self.isConnected = result
+    return result
+
+  def stop(self):
+    if self.isConnected:
+      if self.server:
+        self.server._BaseServer__shutdown_request = True
+        self.server.shutdown()
+        self.server = None
+      self.isConnected = False
+      self.disconnectedEvent(self)
+      logging.debug('[OscServer {0}:{1}] server stopped'.format(self.host, str(self.port)))
+
+  def _onOscMsg(self, addr, *args):
+    logging.debug('[OscServer {0}:{1}] received {2} [{3}]'.format(self.host, self.port, addr, ", ".join(map(lambda x: str(x), args))))
+    self.messageEvent((addr, args))
+
+
+class AddrParser:
+  def __init__(self, addr, scope=''):
+    self.addr = addr
+    self.scope = scope
+  
+  def unscoped(self):
+    return re.compile('^{}'.format(self.scope)).sub('', self.addr)
+
+  def action(self):
+    action = self.unscoped().split('/')[1]
+    logging.info('Action: {}'.format(action))
+    return action
+
+  def path(self):
+    path = '/'+'/'.join(self.unscoped().split('/')[2:])
+    logging.info('Path: {}'.format(path))
+    return path
+    
+
+class OscListener:
+  def __init__(self, paramsList, port=8080, scope="", start=True):
+    self.paramsList = [paramsList] if type(paramsList) == type(Params()) else paramsList
+    self.scope = scope
+    self.server = OscServer(port)
+    self.server.messageEvent += self.onMessage
+
+    if start:
+      self.server.start()
+
+  def __del__(self):
+    self.server.messageEvent -= self.onMessage
+    self.server.stop()
+
+  def onMessage(self, addr, args):
+    parser = AddrParser(addr, scope=self.scope)
+
+    # "/scope/set/Test/name", ('John')
+    if parser.action() == 'set':
+      path = parser.path()
+      p = self.getParamForPath(path)
+      if not p:
+        logging.info('Got invalid set path: {}'.format(addr))
+        return
+      
+      if len(args) < 1:
+        logging.info('Did not get value argument with OSC message {}'.format(addr))
+        return
+
+      value = args[0]
+      logging.debug('Setting {} to {} (osc: {} {})'.format(p.name, value, addr, args))
+      p.set(value)
+
+  def getParamForPath(self, path):
+    for params in self.paramsList:
+      if path.startswith('/{}/'.format(params.name)):
+        parname = re.compile('^/{}/'.format(params.name)).sub('', path)
+        return params.get(parname)
+
+    return None
+
+  def parse(self, addr):
+    parse = {}
+    parse['unscoped'] = re.compile('^{}'.format(self.scope)).sub('', addr)
+    parse['action'] = parse['unscoped'].split('/')[0]
+    parse['path'] = '/'.join(parse['unscoped'].split('/')[1:])
+
+
+
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+  params = Params('Test')
+  with params as pars:
+    pars.add('name', 'Bob')
+    pars.add('age', 21)
+    pars.add('score', 64.05)
+
+
+  oscServer = OscListener(params, 8010, scope='/FooBar', start=False)
+  print('Initial: {}'.format(toDict(params)))
+  oscServer.onMessage('/FooBar/set/Test/name', ['John'])
+  print('After name change: {}'.format(toDict(params)))
+  oscServer.onMessage('/FooBar/set/Test/age', [33])
+  print('After age change: {}'.format(toDict(params)))
+
+
+    
---
+++ SV/params/Params.py
@@ -0,0 +1,80 @@
+from evento import Event
+
+
+class Param:
+  def __init__(self, name, default=None):
+    self.name = name
+    self.default = default
+    self.value = default
+    self.changeEvent = Event()
+
+  def set(self, value):
+    if self.value == value:
+      return
+    self.value = value
+    self.changeEvent(self)
+
+class ParamsBuilder:
+  def __init__(self, params):
+    self.params = params
+
+  def add(self, name, default=None):
+    self.params.append(Param(name, default))
+
+class Params(list):
+  def __init__(self, name=''):
+    self.name = name
+    self.changeEvent = Event()
+
+  def __enter__(self):
+    self.builder = ParamsBuilder(self)
+    return self.builder
+
+  def __exit__(self, type, value, traceback):
+    pass
+
+  def get(self, name):
+    return next(p for p in self if p.name == name)
+
+  def append(self, param):
+    list.append(self, param)
+    def onParamChange(param):
+      self.changeEvent(self)
+    param.changeEvent += onParamChange
+
+def toDict(params):
+  d = {}
+  for p in params:
+    d[p.name] = p.value
+
+  return d
+
+def toSyncDict(params):
+  d = toDict(params)
+
+  def updatevalues(prms):
+    newdict = toDict(prms)
+    d.update(newdict)
+
+  params.changeEvent += updatevalues
+  return d
+
+if __name__ == '__main__':
+  params = Params('Test')
+  with params as pars:
+    pars.add('name', 'Bob')
+    pars.add('delay', 0.05)
+    pars.add('age', 8)
+
+  print('Number of params: {}'.format(len(params)))
+  print('Names: {}'.format(', '.join(map(lambda p: p.name, params))))
+  print('Defaults: {}'.format(', '.join(map(lambda p: str(p.default), params))))
+
+  values = toDict(params)
+  print('Values: {}'.format(values))
+  
+  syncvals = toSyncDict(params)
+  print('SyncValues: {}'.format(syncvals))
+  
+  params.get('name').set('John')
+  print('SyncValues: {}'.format(syncvals))
